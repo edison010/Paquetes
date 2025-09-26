c8d390c65f2fc63cd60fbfa72b1fca719b889b6e
edison010 <edial2005@gmail.com>
2025-09-25 21:02:29 -0500

    Agregar paquetes conversion y contadorVocales y actualizar main.go

--- conversion/conversion.go
+++ conversion/conversion.go
@@ -1,52 +1,30 @@
 package conversion
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
 func Conversion() {
 	var dolares float64
 	var moneda string
-
-	// Tasas de conversión
 	tasaEUR := 0.95     // Euro
 	tasaGBP := 0.82     // Libra esterlina
 	tasaKRW := 1375.00  // Won surcoreano
 	tasaBTC := 0.000015 // Bitcoin
-<<<<<<< HEAD
-	fmt.Println("Ingrese la cantadidad en dólares: ")
-=======
-	fmt.Print("Ingrese la cantidad en dólares: ")
->>>>>>> 45ed494 (Agregar paquetes conversion y contadorVocales y actualizar main.go)
+
+	fmt.Println("Ingrese la cantidad en dólares: ")
 	fmt.Scanln(&dolares)
-	fmt.Print("Conversiones disponibles EUR/GBP/KRW/BTC: ")
+	fmt.Println("Conversiones disponibles EUR/GBP/KRW/BTC : ")
 	fmt.Scanln(&moneda)
-<<<<<<< HEAD
-	moneda = strings.ToUpper(moneda)
-	dolarEuro := dolares * tasaEUR
-	dolarLibras := dolares * tasaGBP
-	dolarWon := dolares * tasaKRW
-	dolarBTC := dolares * tasaBTC
-	switch moneda {
-	case "EUR":
-		fmt.Println("La conversión es de", dolarEuro)
-	case "GBP":
-		fmt.Println("La conversión es de", dolarLibras)
-=======
-	moneda = strings.ToUpper(moneda) // para evitar problemas de mayúsculas/minúsculas
 
 	switch moneda {
 	case "EUR":
-		fmt.Println("La conversión es de", dolares*tasaEUR, "Euros")
+		fmt.Println("La conversión es de", dolares*tasaEUR)
 	case "GBP":
-		fmt.Println("La conversión es de", dolares*tasaGBP, "Libras Esterlinas")
->>>>>>> 45ed494 (Agregar paquetes conversion y contadorVocales y actualizar main.go)
+		fmt.Println("La conversión es de", dolares*tasaGBP)
 	case "KRW":
-		fmt.Println("La conversión es de", dolares*tasaKRW, "Won Surcoreano")
+		fmt.Println("La conversión es de", dolares*tasaKRW)
 	case "BTC":
-		fmt.Println("La conversión es de", dolares*tasaBTC, "Bitcoin")
+		fmt.Println("La conversión es de", dolares*tasaBTC)
 	default:
-		fmt.Println("Moneda no reconocida")
+		fmt.Println("Moneda no válida")
 	}
 }
